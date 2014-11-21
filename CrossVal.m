69cd1fdf728e9e2fbdcb882bba6ecfdde07d2c0d
d-an <dv.daniel@centrum.cz>
2014-11-21 16:22:11 +0100

    hotovo

--- CrossVal.m
+++ CrossVal.m
@@ -1,28 +1,7 @@
-function [ delta, s ] = CrossVal( Name1, Name1L, Par1, Name2, Name2L, Par2, Pat, DOut, k )
+function [ delta, s] = CrossVal( Name1, Name1L, Par1, Name2, Name2L, Par2, Pat, DOut, k )
     
-% docasne promenne kvuli testovani:
-%%
-    In = randn(2, 600);
-    c = In(1,:)-3*In(2,:)>=0.5;
-    Par1 = {[1 1 -1], 1, 10};
-    Par2 = {[1 1 -1], 1, 100};
-    Name1 = 'PLearn';
-    Name1L = 'PRecall';
-    Name2 = 'PLearn';
-    Name2L = 'PRecall';
-    Pat=In;
-    DOut=c;
-    k=5;
-%%
-
-
     n = size(Pat, 2);
     hranice = floor(n/k);
-    rndprm = randperm(n);
-%%
-% zpermutujeme poradi vzoru:
-    Pat = Pat(:,rndprm);
-    DOut = DOut(rndprm);
 %%
 % ulozime si indexy (odkud kam) jednotlivych skupin(foldu):
     indx = [linspace(0,k-1,k)*hranice+1; linspace(1,k,k)*hranice];
@@ -40,18 +19,21 @@ for i=1:k
         DOut_train= DOut(indx(1,2):indx(2,k));
         Pat_test = Pat(:,indx(1,1):indx(2,1));
         DOut_test= DOut(indx(1,1):indx(2,1));
+        n_test = indx(2,1)-indx(1,1)+1; %pro pozdejsi deleni pocet spatne klasifikovanych poctem testovacich dat
         
     elseif i==k
         Pat_train = Pat(:,indx(1,1):indx(2,k-1));
         DOut_train= DOut(indx(1,1):indx(2,k-1));
         Pat_test = Pat(:,indx(1,k):indx(2,k));
         DOut_test= DOut(indx(1,k):indx(2,k));
-    
+        n_test = indx(2,k)-indx(1,k)+1;
+        
     else
         Pat_train = [Pat(:,indx(1,1):indx(2,i-1)) Pat(:,indx(1,i+1):indx(2,k))];
         DOut_train= [DOut(indx(1,1):indx(2,i-1)) DOut(indx(1,i+1):indx(2,k))];
         Pat_test = Pat(:,indx(1,i):indx(2,i));
         DOut_test= DOut(indx(1,i):indx(2,i));
+        n_test = indx(2,i)-indx(1,i)+1;
     end
      
     
@@ -61,11 +43,16 @@ for i=1:k
     Out1 = feval(Name1L, LPar1, Pat_test);
     LPar2 = feval(Name2, Pat_train, DOut_train, Par2);
     Out2 = feval(Name2L, LPar2, Pat_test);
-    err1 = sum(Out1~=DOut_test);
-    err2 = sum(Out2~=DOut_test);
+    err1 = sum(Out1~=DOut_test)/n_test;
+    err2 = sum(Out2~=DOut_test)/n_test;
     % ulozime chyby do err:
     err(i,:)=[err1 err2];
 end
+%spocteme rozdily chyb:
+err = err(:,1)-err(:,2);
+%vratime prumerny rozdil chyb a smerodatnou odchylku:
+delta = mean(err); 
+s = sqrt(sum((err-delta).^2)/(k*(k-1)));
 %%
 end
 
--- skript.m
+++ skript.m
@@ -1,17 +1,24 @@
+stream = RandStream.getGlobalStream; reset(stream)
+In = randn(2, 600);
+c = In(1,:)-3*In(2,:)>=0.5;
+Par1 = {[1 1 -1], 1, 10};
+Par2 = {[1 1 -1], 1, 100};
 
-p = [1 1 -1]
-    
+%porovnani perceptronoveho algoritmu pro 10 a 100 epoch:
+[d,s] = CrossVal('PLearn', 'PRecall', Par1, 'PLearn', 'PRecall', Par2, In, c, 5)
+
+t = tinv(0.975, 5-1);
+
+% 95% interval spolehlivosti pro rozdil chyby:
+[d-s*t, d+s*t]
 
 %%
-x = [1 2 1 4; 
-    -1 -3 4 2]
+stream = RandStream.getGlobalStream; reset(stream)
+In = randi(20, 4, 300);
+c = In(1,:)-3*In(2,:)+2*In(3,:)-In(4,:)>=0;
+Par3 = {[1 1 1 1 -1], 1, 40};
 %%
-    c=[0 1 0 1]
-    %%
-    
-lam=1
-perc_recall(p, x(:,1))
-%%
-extended = [x(:,1); ones(1, size(x(:,1),2))]
+[d,s] = CrossVal('PLearn', 'PRecall', Par3, 'Memorizer', 'MemorizerRecall', Par3, In, c, 6)
+t = tinv(0.975, 6-1)
 
-p*extended
+%%
